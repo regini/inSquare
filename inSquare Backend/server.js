9f6027872bf44bdb97549989361bacb658611f97
regini <nr@regini.eu>
2016-01-04 13:04:44 +0100

    ElasticSearch Update

--- inSquare Backend/server.js
+++ inSquare Backend/server.js
@@ -1,7 +1,9 @@
+
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mysql = require('mysql');
+var elasticsearch = require('elasticsearch');
 var vsprintf = require("sprintf-js").vsprintf;
 
 /*
@@ -23,21 +25,25 @@ http.listen(server_port, server_ip_address, function () {
 });
 
 //CONNESSIONE AL DATABASE
-var connection = mysql.createConnection({
-    host     : db_host,
-    port     : db_port,
-    user     : db_username,
-    password : db_password,
-    database : db_name
+var client = new elasticsearch.Client({
+  host: 'http://elastic-insquare.rhcloud.com',
+  log: 'trace'
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
+client.ping({
+  // ping usually has a 3000ms timeout 
+  requestTimeout: Infinity,
+ 
+  // undocumented params are appended to the query string 
+  hello: "elasticsearch!"
+}, function (error) {
+  if (error) {
+    console.trace('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
   }
-  console.log('connected as id ' + connection.threadId);
 });
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -49,7 +55,21 @@ io.on('connection', function(socket){
   io.emit('users connected', socketCount);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    connection.query('INSERT INTO `message`(`text`) VALUES (?)',msg);
+    
+
+  client.create({
+  index: 'message',
+  type: 'messages',
+  body: {
+        text: msg,
+        user: "Nik",
+        square:"Roma",
+        timestamp: new Date()
+  }
+});
+
+
+
   });
   socket.on('disconnect', function(){
     socketCount--;
@@ -57,6 +77,9 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
 });
+
+
+
 
 //POST request alla home che risponde con hello post
 app.post('/', function (req, res) {
@@ -71,8 +94,7 @@ app.post('/', function (req, res) {
 app.post('/inviaMessaggio', function(req,res) {
     console.log("richiesta di invio di un messaggio");
     var message = req.query.message;
-    var query = vsprintf('INSERT INTO `Message`(`text`) VALUES ("%s")',[message]);
-    console.log(query);
+    
     connection.query(query, function(error) {
         if (error) {
             res.send(error);
@@ -84,18 +106,30 @@ app.post('/inviaMessaggio', function(req,res) {
 });
 
 
+
+
 /*
     GET request per ottenere i messaggi nella table
 */
 app.get('/getMessaggi', function(req,res) {
     console.log("chiamata a getMessaggi");
-    connection.query('SELECT * FROM Message', function(err, result) {
-      if (err)
-        res.send(error);
-      else
-        var response = '';
-        for (var i = 0; i < result.length; i++)
-            response += result[i].text + '<br>'
-        res.send(response);
-    });
-})
+
+    client.search({
+    index: 'message',
+    type: 'messages',
+    body: {
+     query: {
+      match_all:{}
+      }
+    }
+    }).then(function (resp) {
+    var hits = resp.hits.hits;
+    res.send(resp);
+}, function (err) {
+    console.trace(err.message);
+    res.send(err);
+});
+      
+});
+
+
