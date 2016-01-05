135c90b03fd3e5a861e0a6dab46d1bbbeca66352
regini <nr@regini.eu>
2016-01-05 15:30:37 +0100

    Update Auth Backend

--- inSquare Backend/server.js
+++ inSquare Backend/server.js
@@ -4,18 +4,31 @@ var io = require('socket.io')(http);
 var elasticsearch = require('elasticsearch');
 var vsprintf = require("sprintf-js").vsprintf;
 
+var passport = require('passport');
+var strategy = require('./setup-passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var requiresLogin = require('./requiresLogin');
+
 /*
   Setup delle variabili prese dall'environment
 */
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var db_host = process.env.OPENSHIFT_MYSQL_DB_HOST;  // ||localhost da testare
-var db_port = process.env.OPENSHIFT_MYSQL_DB_PORT; //idem
-var db_username = process.env.OPENSHIFT_MYSQL_DB_USERNAME;
-var db_password = process.env.OPENSHIFT_MYSQL_DB_PASSWORD;
-var db_name = process.env.OPENSHIFT_GEAR_NAME;
 
 var socketCount = 0;
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'Hn98wRqgho210gbhs7UVNdQBzviVarMqJq51Md10QtlysDWYM0vs_6DLJLL8Ek5b',
+  resave: false,
+  saveUninitialized: false
+ }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // CONNESSIONE AL SERVER
 http.listen(server_port, server_ip_address, function () {
@@ -42,9 +55,17 @@ client.ping({
   }
 });
 
+app.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: 'www.google.com' }),
+  function(req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.sendFile(__dirname + '/chat.html');
+  });
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
@@ -53,36 +74,22 @@ io.on('connection', function(socket){
   io.emit('users connected', socketCount);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-
-
-  client.create({
-  index: 'message',
-  type: 'messages',
-  body: {
+    client.create({
+      index: 'message',
+      type: 'messages',
+      body: {
         text: msg,
         user: "Nik",
         square:"Roma",
         timestamp: new Date()
-  }
-});
-
-
-
+      }
+    });
   });
   socket.on('disconnect', function(){
     socketCount--;
     io.emit('users connected', socketCount)
     console.log('user disconnected');
   });
-});
-
-
-
-
-//POST request alla home che risponde con hello post
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
 });
 
 /*
@@ -103,29 +110,51 @@ app.post('/inviaMessaggio', function(req,res) {
     });
 });
 
+app.get('/getMessageHistory', function(req,res) {
+  console.log("chiamata a getMessageHistory");
+  client.search({
+    index: 'message',
+    type: 'messages',
+    body: {
+      query: {
+        match_all:{}
+      }
+    }
+    }).then(function (resp) {
+      var hits = resp.hits.hits;
+      res.send(resp);
+    }, function (err) {
+      console.trace(err.message);
+      res.send(err);
+    });
+});
 
-
+app.get('/user',
+  requiresLogin,
+  function (req, res) {
+    res.render('user', {
+      user: req.user
+    });
+  });
 
 /*
     GET request per ottenere i messaggi nella table
 */
-app.get('/getMessaggi', function(req,res) {
+app.get('/getMessaggi', requiresLogin, function(req,res) {
     console.log("chiamata a getMessaggi");
-
     client.search({
-    index: 'message',
-    type: 'messages',
-    body: {
-     query: {
-      match_all:{}
+      index: 'message',
+      type: 'messages',
+      body: {
+        query: {
+          match_all:{}
+        }
       }
-    }
     }).then(function (resp) {
-    var hits = resp.hits.hits;
-    res.send(resp);
-}, function (err) {
-    console.trace(err.message);
-    res.send(err);
-});
-
+      var hits = resp.hits.hits;
+      res.send(resp);
+    }, function (err) {
+      console.trace(err.message);
+      res.send(err);
+    });
 });
---
+++ inSquare Backend/setup-passport.js
@@ -0,0 +1,27 @@
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
+
+var strategy = new Auth0Strategy({
+    domain:       'insquare.eu.auth0.com',
+    clientID:     '4QnTav7OyCqXBsPHEUgqGxC8xl69Urmv',
+    clientSecret: 'Hn98wRqgho210gbhs7UVNdQBzviVarMqJq51Md10QtlysDWYM0vs_6DLJLL8Ek5b',
+    callbackURL:  '/callback'
+  }, function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  });
+
+passport.use(strategy);
+
+// This is not a best practice, but we want to keep things simple for now
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+module.exports = strategy;
