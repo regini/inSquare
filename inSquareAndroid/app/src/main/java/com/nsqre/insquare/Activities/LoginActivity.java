f3d50c7923799b74f40d7cba476c9ef358b03dca
Emanuele Grassi <Emanuele Grassi>
2016-01-15 18:27:34 +0100

    google login

--- inSquareAndroid/app/src/main/java/com/nsqre/insquare/Activities/LoginActivity.java
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Activities/LoginActivity.java
@@ -1,8 +1,15 @@
 package com.nsqre.insquare.Activities;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageInstaller;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -19,12 +26,33 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 import com.google.gson.Gson;
 import com.nsqre.insquare.R;
 import com.nsqre.insquare.Utilities.User;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -33,14 +61,17 @@ public class LoginActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
     private String userId;
     private String accessToken;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 211;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
-        info = (TextView)findViewById(R.id.info);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
+        info = (TextView) findViewById(R.id.info);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
 
         callbackManager = CallbackManager.Factory.create();
         final Button loginfb = (Button) findViewById(R.id.buttonfb);
@@ -51,11 +82,11 @@ public class LoginActivity extends AppCompatActivity {
                 userId = loginResult.getAccessToken().getUserId();
                 accessToken = loginResult.getAccessToken().getToken();
                 info.setText(
-                    "User ID: "
-                    + userId
-                    + "\n" +
-                    "Auth Token: "
-                    + accessToken
+                        "User ID: "
+                                + userId
+                                + "\n" +
+                                "Auth Token: "
+                                + accessToken
                 );
             }
 
@@ -70,6 +101,43 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        Log.d("google", "connesso il client");
+                        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                        startActivityForResult(signInIntent, RC_SIGN_IN);
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+        //accesso alla chat
         loginfb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,13 +151,7 @@ public class LoginActivity extends AppCompatActivity {
                             @Override
                             public void onResponse(String response) {
                                 loginfb.setText("Response is: " + response);
-                                User user = new User();
-                                //// TODO: 15/01/2016  SOME JSON MAGIC MUST HAPPEN HERE
-                                //Gson gson = new Gson();
-                                //user = gson.fromJson(response, user.getClass());
-                                Intent intent = new Intent(getApplicationContext(), FabActivity.class);
-                                intent.putExtra("CURRENT_USER", user);
-                                startActivity(intent);
+                                json2login(response);
                             }
                         }, new Response.ErrorListener() {
                     @Override
@@ -97,35 +159,14 @@ public class LoginActivity extends AppCompatActivity {
                         loginfb.setText(error.toString());
                     }
                 }) {
-                    /*
                     @Override
-                    public Map<String, String> getHeaders() throws AuthFailureError {
+                    protected Map<String, String> getParams() {
                         Map<String, String> params = new HashMap<String, String>();
-                        params.put("userid", userId);
-                        params.put("token", accessToken);
-
-                        return params;
-                    }
-                    */
-                    @Override
-                    protected Map<String,String> getParams(){
-                        Map<String,String> params = new HashMap<String, String>();
                         params.put("access_token", accessToken);
 
                         return params;
                     }
-
-                    /*
-                    @Override
-                    public Map<String, String> getHeaders() throws AuthFailureError {
-                        Map<String,String> params = new HashMap<String, String>();
-                        params.put("Content-Type","application/x-www-form-urlencoded");
-                        return params;
-                    }
-                    */
                 };
-
-                // Add the request to the RequestQueue.
                 queue.add(stringRequest);
             }
         });
@@ -133,13 +174,161 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void requestPermissionAccounts() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.GET_ACCOUNTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.GET_ACCOUNTS)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.GET_ACCOUNTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void json2login(String jsonUser) {
+        User user = new User();
+        //// TODO: 15/01/2016  SOME JSON MAGIC MUST HAPPEN HERE
+        //Gson gson = new Gson();
+        //user = gson.fromJson(response, user.getClass());
+        Intent intent = new Intent(getApplicationContext(), FabActivity.class);
+        intent.putExtra("CURRENT_USER", user);
+        startActivity(intent);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("google+", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            accessToken = acct.getIdToken();
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+            String url = "http://recapp-insquare.rhcloud.com/auth/google/token";
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("google+response", "Response is: " + response);
+                            json2login(response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("google+response", error.toString());
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("access_token", accessToken);
+
+                    return params;
+                }
+            };
+            queue.add(stringRequest);
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        accessToken = AccessToken.getCurrentAccessToken().getToken(); //SE UTENTE HA GIà LOGGATO UN'ALTRA VOLTA, RITROVA IL TOKEN.
-        Log.d("token", accessToken);
+        //mGoogleApiClient.connect();
+        try {
+            accessToken = AccessToken.getCurrentAccessToken().getToken(); //SE UTENTE HA GIà LOGGATO UN'ALTRA VOLTA, RITROVA IL TOKEN.
+            Log.d("token", accessToken);
+        }
+        catch (Exception e) {
+            Log.d("token", e.toString());
+        }
+
+        /*
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW,
+                "Login Page",
+                // If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse(""),
+                // Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.nsqre.insquare.LoginActivity/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
+        */
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        /*
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW,
+                "Login Page",
+                //If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse(""),
+                //  Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.nsqre.insquare.LoginActivity/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
+        mGoogleApiClient.disconnect();
+        */
     }
 }
