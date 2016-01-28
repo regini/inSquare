bb230c0e17910a09e621e48128acc82ec012f548
umberto-sonnino <Rimher@gmail.com>
2016-01-27 20:49:09 -0800

    Added a first map layer

--- inSquareAndroid/app/src/main/java/com/nsqre/insquare/Activities/LoginActivity.java
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Activities/LoginActivity.java
@@ -15,10 +15,13 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
@@ -27,9 +30,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.gson.Gson;
 import com.nsqre.insquare.R;
 import com.nsqre.insquare.Utilities.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,12 +46,13 @@ public class LoginActivity extends AppCompatActivity
 {
 
     private static final String TAG = "LoginActivity";
-    
+
     // Facebook Login
     private LoginButton fbLoginButton;
     private CallbackManager fbCallbackManager;
     private String fbUserId;
     private String fbAccessToken;
+    private AccessTokenTracker fbTokenTracker;
     // ==============
 
     // Google Login
@@ -56,44 +64,61 @@ public class LoginActivity extends AppCompatActivity
 //    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 211;
     // ============
 
+    private Button goChatButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
-        fbLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
 
         fbCallbackManager = CallbackManager.Factory.create();
-        final Button goChatButton = (Button) findViewById(R.id.chat_now_button);
+
+        fbLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        // Permessi da richiedere durante il login
+        fbLoginButton.setReadPermissions("public_profile", "email", "user_friends");
+
+        // Controlla che un utente si sia gia' loggato
+        fbTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if(AccessToken.getCurrentAccessToken() != null)
+                {
+                    Log.d(TAG, "onCurrentAccessTokenChanged: Facebook already logged in!");
+                    startInSquare();
+                }
+            }
+        };
+
+        gLoginButton = (Button) findViewById(R.id.google_login_button);
 
         fbLoginButton.registerCallback(fbCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                fbUserId = loginResult.getAccessToken().getUserId();
-                fbAccessToken = loginResult.getAccessToken().getToken();
-                Log.d("FACEBOOK", "User ID: "
-                        + fbUserId
-                        + "\n" +
-                        "Auth Token: "
-                        + fbAccessToken);
+                if(AccessToken.getCurrentAccessToken() != null)
+                {
+                    requestData();
+                }
             }
 
             @Override
             public void onCancel() {
-                Log.d("FACEBOOK", "Login attempt canceled.");
+                Log.d(TAG + "Facebook", "Login attempt canceled.");
             }
 
             @Override
             public void onError(FacebookException e) {
-                Log.d("FACEBOOK", e.toString());
+                Log.d(TAG + "Facebook", e.toString());
             }
         });
 
         // setup delle opzioni di google+
         gSo = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
                 .requestIdToken(getString(R.string.server_client_id))
                 .requestEmail()
                 .build();
+
 
         // builda il client e prova a chiamare startActivityForResult, se il login è stato già fatto precedentemente
         // il codice è quello corretto e l'app passa subito a fabactivity(ma tanto non lo fa perchè la post non funziona)
@@ -101,20 +126,28 @@ public class LoginActivity extends AppCompatActivity
                 .addConnectionCallbacks(this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gSo)
                 .build();
-
         gApiClient.connect();
 
+        // Se il login e' gia' in cache, fai partire le chat
+        if(getGoogleSignIn().isDone())
+        {
+            startInSquare();
+            gLoginButton.setText(R.string.google_logout_string);
+        }
+
         // Non avendo ancora effettuato il Login con Google
-        gLoginButton = (Button) findViewById(R.id.google_login_button);
         gLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                googleDialogSignIn();
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(gApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
 
         // accesso alla chat
         // bottone che invia il token a facebook e ti fa entrare nella fabactivity
+        goChatButton = (Button) findViewById(R.id.chat_now_button);
+
         goChatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -153,13 +186,16 @@ public class LoginActivity extends AppCompatActivity
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         // Ritorno dal login di Google+
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
+            startInSquare();
         } else {
             // Ritorno dal Login di Facebook
             fbCallbackManager.onActivityResult(requestCode, resultCode, data);
+            startInSquare();
         }
     }
 
@@ -184,9 +220,7 @@ public class LoginActivity extends AppCompatActivity
 
     @Override
     public void onConnected(Bundle bundle) {
-        Log.d("GOOGLE", "connesso il client");
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(gApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        Log.d(TAG + " Google", "Client connesso con successo!");
     }
 
     @Override
@@ -216,12 +250,18 @@ public class LoginActivity extends AppCompatActivity
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    private void startInSquare()
+    {
+        Intent i = new Intent(LoginActivity.this, MapActivity.class);
+        startActivity(i);
+    }
+
     //metodo che elabora il json preso dalle post, crea l'oggetto user e va a fabactivity
     private void json2login(String jsonUser) {
         User user;
         Gson gson = new Gson();
         user = gson.fromJson(jsonUser, User.class);
-        Intent intent = new Intent(getApplicationContext(), FabActivity.class);
+        Intent intent = new Intent(getApplicationContext(), MapActivity.class);
         intent.putExtra("CURRENT_USER", user);
         startActivity(intent);
     }
@@ -229,14 +269,22 @@ public class LoginActivity extends AppCompatActivity
     //quando il login a g+ va a buon fine esegue questo, dove c'è la post(che da errore 500)
     private void handleSignInResult(GoogleSignInResult result) {
 
-        Log.d(TAG + " - Google", "Success? " + result.isSuccess());
+        Log.d("Google" + TAG, "Success? " + result.isSuccess() + "\nStatus Code: " + result.getStatus().getStatusCode());
 
-        if (result.isSuccess()) {
+        if (result.isSuccess())
+        {
 
             GoogleSignInAccount acct = result.getSignInAccount();
             gAccessToken = acct.getIdToken();
 
-            // Instantiate the RequestQueue.
+            Log.d(TAG, "Login was a success: " + acct.getDisplayName() + ": " + acct.getEmail());
+            Log.d(TAG, "Token is: " + acct.getIdToken());
+
+            gLoginButton.setText(R.string.google_logout_string);
+
+            // Commentato al momento perche' non funziona [Umberto]
+
+            /*// Instantiate the RequestQueue.
             RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
             String url = "http://recapp-insquare.rhcloud.com/auth/google/token";
 
@@ -261,7 +309,44 @@ public class LoginActivity extends AppCompatActivity
                     return params;
                 }
             };
-            queue.add(stringRequest);
+            queue.add(stringRequest);*/
         }
+    }
+
+    private void requestData()
+    {
+        // Creazione di una nuova richiesta al grafo di Facebook per le informazioni necessarie
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Log.d(TAG, "Hello Facebook!" + response.toString());
+
+                try
+                {
+                    String nome = object.getString("name");
+                    String email = object.getString("email");
+                    String gender = object.getString("gender");
+
+                    Log.d(TAG, "Name: " + nome
+                                + " email: " + email
+                                + " Gender: " + gender);
+
+                }catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Bundle params = new Bundle();
+        params.putString("fields", "id,name,gender,email,picture");
+        graphRequest.setParameters(params);
+        graphRequest.executeAsync();
+    }
+
+    private OptionalPendingResult<GoogleSignInResult> getGoogleSignIn()
+    {
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(gApiClient);
+
+        return opr;
     }
 }
---
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Activities/MapActivity.java
@@ -0,0 +1,15 @@
+package com.nsqre.insquare.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nsqre.insquare.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
