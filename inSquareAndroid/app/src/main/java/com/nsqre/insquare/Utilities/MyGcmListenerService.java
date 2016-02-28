26f2b6674d90c42acd3e34c36d70c800094abcd9
Stefano Silvi <sambora93@gmail.com>
2016-02-28 19:08:49 +0100

    connessione gcm

--- inSquareAndroid/app/src/main/java/com/nsqre/insquare/Utilities/MyGcmListenerService.java
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Utilities/MyGcmListenerService.java
@@ -88,7 +88,7 @@ public class MyGcmListenerService extends GcmListenerService {
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.nsqre_map_pin)
                 .setContentTitle("GCM Message")
                 .setContentText(message)
                 .setAutoCancel(true)
