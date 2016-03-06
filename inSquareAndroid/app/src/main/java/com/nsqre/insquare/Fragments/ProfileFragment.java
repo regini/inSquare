d5147861b744a50b3bbb06302d62cbbfec2d1668
umberto-sonnino <Rimher@gmail.com>
2016-03-06 17:57:14 +0100

    Alcune modifiche al Fragment del profilo

--- inSquareAndroid/app/src/main/java/com/nsqre/insquare/Fragments/ProfileFragment.java
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Fragments/ProfileFragment.java
@@ -19,7 +19,10 @@ import com.nsqre.insquare.InSquareProfile;
 import com.nsqre.insquare.R;
 import com.nsqre.insquare.Utilities.DownloadImageTask;
 import com.nsqre.insquare.Utilities.ImageConverter;
+import com.nsqre.insquare.Utilities.Square;
 import com.nsqre.insquare.Utilities.SquareAdapter;
+
+import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment implements TabLayout.OnTabSelectedListener {
 
@@ -58,8 +61,6 @@ public class ProfileFragment extends Fragment implements TabLayout.OnTabSelected
         super.onStart();
         //TODO sostituire con un placeholder del profilo
         //setta il placeholder, mentre attende il download dell'immagine
-        if(tabLayout.getTabCount() == 0)
-            setupTabLayout();
     }
 
     @Override
@@ -86,27 +87,19 @@ public class ProfileFragment extends Fragment implements TabLayout.OnTabSelected
             new DownloadImageTask(profileImage).execute(userProfile.getPictureUrl());
         username.setText(userProfile.getUsername());
 
+        setupTabLayout();
 
         return v;
     }
 
     private void setupTabLayout() {
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-        tabLayout.addTab(tabLayout.newTab().setText(TAB_FAVOURITE));
-        tabLayout.addTab(tabLayout.newTab().setText(TAB_OWNED));
+        TabLayout.Tab favouritesTab = tabLayout.newTab().setText(TAB_FAVOURITE);
+        tabLayout.addTab(favouritesTab, 0); // Il numero specifica dove
+        tabLayout.addTab(tabLayout.newTab().setText(TAB_OWNED), 1);
         tabLayout.setOnTabSelectedListener(this);
 
-        //gestisce il caso in cui non ho piazze create
-        //tablayout.gettab con l'index e poi .select(), non triggera onTabSelected
-        if (!InSquareProfile.favouriteSquaresList.isEmpty()) {
-//        if (!InSquareProfile.ownedSquaresList.isEmpty()) {
-            squaresList.setAdapter(adapterFavourite);
-//            squaresList.setAdapter(adapterOwned);
-            emptyText.setVisibility(View.INVISIBLE);
-        } else {
-            emptyText.setVisibility(View.VISIBLE);
-            emptyText.setText(getString(R.string.profile_empty_owned));
-        }
+        onTabSelected(favouritesTab);
     }
 
     @Override
@@ -122,34 +115,35 @@ public class ProfileFragment extends Fragment implements TabLayout.OnTabSelected
     @Override
     public void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause: I've just paused!");
     }
 
     @Override
     public void onTabSelected(TabLayout.Tab tab) {
         Log.d(TAG, "onTabSelected: I've selected " + tab.getText());
 
-        adapterFavourite.notifyDataSetChanged();
-        adapterOwned.notifyDataSetChanged();
-
         if (tab.getText() == TAB_OWNED) {
-            if (!InSquareProfile.ownedSquaresList.isEmpty()) {
-                squaresList.setAdapter(adapterOwned);
-                emptyText.setVisibility(View.INVISIBLE);
-            } else {
-                emptyText.setVisibility(View.VISIBLE);
-                emptyText.setText(getString(R.string.profile_empty_owned));
-            }
+            fillTab(InSquareProfile.ownedSquaresList, adapterOwned, getString(R.string.profile_empty_owned));
         }
 
         if(tab.getText() == TAB_FAVOURITE) {
-            if (!InSquareProfile.favouriteSquaresList.isEmpty()) {
-                squaresList.setAdapter(adapterFavourite);
-                emptyText.setVisibility(View.INVISIBLE);
-            } else {
-                emptyText.setVisibility(View.VISIBLE);
-                emptyText.setText(getString(R.string.profile_empty_favourite));
-            }
+            fillTab(InSquareProfile.favouriteSquaresList, adapterFavourite, getString(R.string.profile_empty_favourite));
+        }
+        adapterFavourite.notifyDataSetChanged();
+        adapterOwned.notifyDataSetChanged();
+    }
+
+    private void fillTab(ArrayList<Square> list, SquareAdapter listAdapter, String message)
+    {
+        if(list.isEmpty())
+        {
+            squaresList.setVisibility(View.INVISIBLE);
+            emptyText.setVisibility(View.VISIBLE);
+            emptyText.setText(message);
+        }else
+        {
+            squaresList.setVisibility(View.VISIBLE);
+            squaresList.setAdapter(listAdapter);
+            emptyText.setVisibility(View.INVISIBLE);
         }
     }
 
