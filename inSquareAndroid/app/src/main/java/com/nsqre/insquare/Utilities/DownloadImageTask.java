f312c060292689e01d8896ea56dd1a9eba71aff0
Emanuele Grassi <Emanuele Grassi>
2016-03-10 12:24:58 +0100

    aggiunto il salvataggio in locale dell'immagine di profilo dell'utente

--- inSquareAndroid/app/src/main/java/com/nsqre/insquare/Utilities/DownloadImageTask.java
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Utilities/DownloadImageTask.java
@@ -1,11 +1,19 @@
 package com.nsqre.insquare.Utilities;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.nsqre.insquare.Activities.MapActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -13,9 +21,11 @@ import java.io.InputStream;
  */
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
     ImageView bmImage;
+    MapActivity activity;
 
-    public DownloadImageTask(ImageView bmImage) {
+    public DownloadImageTask(ImageView bmImage, MapActivity mapActivity) {
         this.bmImage = bmImage;
+        this.activity = mapActivity;
     }
 
     protected Bitmap doInBackground(String... urls) {
@@ -34,5 +44,6 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
     protected void onPostExecute(Bitmap result) {
         Bitmap circularBitmap = ImageConverter.getRoundedCornerBitmap(result, 100);
         bmImage.setImageBitmap(circularBitmap);
+        activity.saveToInternalStorage(circularBitmap);
     }
 }
