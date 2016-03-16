12a37616fc39d69a4109a8d205999a543d2292d5
umberto-sonnino <Rimher@gmail.com>
2016-03-16 22:45:50 +0100

    Fix ai pin sulla mappa

--- inSquareAndroid/app/src/main/java/com/nsqre/insquare/Fragments/MapFragment.java
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Fragments/MapFragment.java
@@ -240,36 +240,6 @@ public class MapFragment extends Fragment
         LocalBroadcastManager.getInstance(getActivity().getApplicationContext()).registerReceiver(mMessageReceiver,
                 new IntentFilter("update_squares"));
         InSquareProfile.addListener(this);
-        if(mGoogleMap != null && squareHashMap.size() > 0)
-        {
-            mGoogleMap.clear();
-
-            // Riempi la mappa
-            HashMap<String, Square> squarePins = new HashMap<>();
-            for(Square s : squareHashMap.values())
-            {
-                squarePins.put(s.getId(), s);
-            }
-            squareHashMap.clear();
-            for(Square closeSquare : squarePins.values())
-            {
-                LatLng coords = new LatLng(closeSquare.getLat(), closeSquare.getLon());
-                Marker m = createSquarePin(coords, closeSquare.getName());
-                squareHashMap.put(m, closeSquare);
-            }
-            Log.d(TAG, "onResume: map has been refilled!");
-            // Fine refill
-
-            downloadAndInsertPins(PIN_DOWNLOAD_RADIUS_MAX, mGoogleMap.getCameraPosition().target);
-            if(bottomSheetSeparator.getVisibility() == View.VISIBLE) {
-                bottomSheetUpperLinearLayout.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startChatActivity(mLastSelectedSquare);
-                    }
-                });
-            }
-        }
     }
 
     @Override
@@ -738,7 +708,39 @@ public class MapFragment extends Fragment
             LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
             if(locationManager != null && googleMap != null)
             {
+                Log.d(TAG, "onMapReady: mappa != null e hashMap con roba");
                 mGoogleMap = googleMap;
+                if(mGoogleMap != null && squareHashMap.size() > 0)
+                {
+                    // Riempi la mappa
+                    HashMap<String, Square> squarePins = new HashMap<>();
+                    for(Square s : squareHashMap.values())
+                    {
+                        squarePins.put(s.getId(), s);
+                    }
+                    squareHashMap.clear();
+                    mGoogleMap.clear();
+                    for(Square closeSquare : squarePins.values())
+                    {
+                        LatLng coords = new LatLng(closeSquare.getLat(), closeSquare.getLon());
+                        Marker m = createSquarePin(coords, closeSquare.getName());
+                        squareHashMap.put(m, closeSquare);
+                    }
+                    Log.d(TAG, "onResume: map has been refilled!");
+                    // Fine riempimento
+
+
+                    // Download dei nuovi pin
+                    downloadAndInsertPins(PIN_DOWNLOAD_RADIUS_MAX, mGoogleMap.getCameraPosition().target);
+                    if(bottomSheetSeparator.getVisibility() == View.VISIBLE) {
+                        bottomSheetUpperLinearLayout.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startChatActivity(mLastSelectedSquare);
+                            }
+                        });
+                    }
+                }
             }
         }
 
