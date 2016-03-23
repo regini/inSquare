b315acf279fee7563b29d7e45860fa7c2aa050bf
Niccolò Regini <nr@regini.eu>
2016-03-23 23:57:12 +0100

    Revert "Fix"
    This reverts commit abe746271a8086fbc74247a328b571f3659ffa21.

--- inSquareAndroid/app/src/main/java/com/nsqre/insquare/Utilities/Photo/helpers/NotificationHelper.java
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Utilities/Photo/helpers/NotificationHelper.java
@@ -21,6 +21,7 @@ public class NotificationHelper {
     public final static String TAG = NotificationHelper.class.getSimpleName();
 
     private WeakReference<Context> mContext;
+    private static String fotoURL;
 
     public NotificationHelper(Context context) {
         this.mContext = new WeakReference<>(context);
@@ -71,6 +72,7 @@ public class NotificationHelper {
                 (NotificationManager) mContext.get().getSystemService(Context.NOTIFICATION_SERVICE);
 
         mNotificationManager.notify(mContext.get().getString(R.string.app_name).hashCode(), mBuilder.build());
+        fotoURL = response.data.link;
     }
 
     public void createFailedUploadNotification() {
@@ -87,5 +89,9 @@ public class NotificationHelper {
                 (NotificationManager) mContext.get().getSystemService(Context.NOTIFICATION_SERVICE);
 
         mNotificationManager.notify(mContext.get().getString(R.string.app_name).hashCode(), mBuilder.build());
+    }
+
+    public static String getFotoURL() {
+        return fotoURL;
     }
 }
