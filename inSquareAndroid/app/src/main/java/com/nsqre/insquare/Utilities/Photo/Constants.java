1cc3b7ffc3e20bcd37898db126ea4e475cc3250e
Niccolò Regini <nr@regini.eu>
2016-03-23 20:37:09 +0100

    Prima integrazione di messaggi con foto

---
+++ inSquareAndroid/app/src/main/java/com/nsqre/insquare/Utilities/Photo/Constants.java
@@ -0,0 +1,32 @@
+package com.nsqre.insquare.Utilities.Photo;
+
+/**
+ * Created by AKiniyalocts on 2/23/15.
+ */
+public class Constants {
+    /*
+      Logging flag
+     */
+    public static final boolean LOGGING = false;
+
+    /*
+      Your imgur client id. You need this to upload to imgur.
+
+      More here: https://api.imgur.com/
+     */
+    public static final String MY_IMGUR_CLIENT_ID = "d0b5cc3101b3308";
+    public static final String MY_IMGUR_CLIENT_SECRET = "";
+
+    /*
+      Redirect URL for android.
+     */
+    public static final String MY_IMGUR_REDIRECT_URL = "http://android";
+
+    /*
+      Client Auth
+     */
+    public static String getClientAuth() {
+        return "Client-ID " + MY_IMGUR_CLIENT_ID;
+    }
+
+}
